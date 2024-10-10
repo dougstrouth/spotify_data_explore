2bf6c8a0117b3964446fe1a04505c005df04003a
Doug Strouth <dougstrouth@gmail.com>
2024-10-10 13:01:18 -0500

    stuff

--- libraries/duckdb_utils.py
+++ libraries/duckdb_utils.py
@@ -2,9 +2,7 @@ import logging
 import duckdb
 import glob
 import os
-from libraries.general_utilities import setup_logging
 
-setup_logging()
 
 
 def execute_sql_from_file(sql_file, folder_path, duckdb_file):
@@ -25,7 +23,7 @@ def execute_sql_from_file(sql_file, folder_path, duckdb_file):
     conn.close()
 
 
-def query_duckdb_metadata(duckdb_file_path, schema="check_csv"):
+def query_duckdb_metadata(duckdb_file_path, schema):
     """Query DuckDB's information_schema.columns view and return a DataFrame."""
     try:
         con = duckdb.connect(duckdb_file_path)
@@ -49,108 +47,3 @@ def query_duckdb_metadata(duckdb_file_path, schema="check_csv"):
         raise
     finally:
         con.close()
-
-
-def create_pbp_from_csvs(duckdb_file, schema_name, folder_path):
-    """Create tables in DuckDB from CSV files stored in folders with specific format."""
-
-    conn = duckdb.connect(duckdb_file)
-    conn.execute(
-        f"CREATE SCHEMA IF NOT EXISTS {schema_name}"
-    )  # List all subdirectories (i.e., table folders) in the given folder_path
-    table_folders = [
-        d for d in glob.glob(os.path.join(folder_path, "*")) if os.path.isdir(d)
-    ]
-
-    for table_folder in table_folders:
-        # Extract table name from the folder name
-        table_name = os.path.basename(table_folder)
-
-        # Construct the pattern to include all CSV files within this folder
-        pattern = os.path.join(table_folder, "*.csv")
-
-        # Create SQL statement to read all CSV files in the folder and create a table
-        if table_name == "play_by_play":
-            # For the 'play_by_play' table, specify column types explicitly
-            sql = f"""
-            DROP TABLE IF EXISTS {schema_name}.{table_name};
-
-            CREATE TABLE {schema_name}.{table_name} AS
-            SELECT * FROM read_csv('{pattern}',
-                                   union_by_name=true,
-                                   types={{'time_of_day': 'TIME'}});
-            """
-        elif table_name == "weekly_rosters":
-            # For the 'weekly_rosters' table, specify column types explicitly
-            sql = f"""
-            DROP TABLE IF EXISTS {schema_name}.{table_name};
-
-            CREATE TABLE {schema_name}.{table_name} AS
-            SELECT * FROM read_csv('{pattern}',
-                                   union_by_name=true,
-                                   types={{'jersey_number': 'VARCHAR'}});
-            """
-        else:
-            # For other tables, no specific column types are set
-            sql = f"""
-            DROP TABLE IF EXISTS {schema_name}.{table_name};
-
-            CREATE TABLE {schema_name}.{table_name} AS
-            SELECT * FROM read_csv('{pattern}',
-                                   union_by_name=true);
-            """
-
-        # Execute SQL statement
-        try:
-            conn.execute(sql)
-            print(f"Table {table_name} created successfully.")
-        except Exception as e:
-            print(f"Failed to create table {table_name}: {e}")
-
-    conn.close()
-
-
-
-def create_tables_from_csvs(duckdb_file, schema_name, folder_path):
-    """Create tables in DuckDB from CSV files directly within the provided folder."""
-
-    # Construct the pattern to search for CSV files in the specified folder only
-    pattern = os.path.join(folder_path, "*.csv")
-    csv_files = glob.glob(pattern)
-
-    conn = duckdb.connect(duckdb_file)
-    conn.execute(f"CREATE SCHEMA IF NOT EXISTS {schema_name}")
-    for csv_file in csv_files:
-        # Extract table name from the CSV file name
-        table_name = os.path.splitext(os.path.basename(csv_file))[0]
-
-        # Create SQL statement to read CSV and create a table
-        sql = f"""
-        DROP TABLE IF EXISTS {schema_name}.{table_name};
-
-        CREATE TABLE {schema_name}.{table_name} AS
-        SELECT * FROM read_csv('{csv_file}', union_by_name=true);
-        """
-
-        # Execute SQL statement
-        try:
-            conn.execute(sql)
-            print(f"Table {table_name} created successfully.")
-        except Exception as e:
-            print(f"Failed to create table {table_name}: {e}")
-
-    conn.close()
-
-
-folder_path = (
-    "/Users/dougstrouth/Documents/Code/datasets/sports/NFL/raw_data/play_by_play"
-)
-duckdb_file = (
-    "/Users/dougstrouth/Documents/Code/datasets/sports/NFL/raw_data/nfl_data.duckdb"
-)
-schema = "play_by_play"
-query_duckdb_metadata(duckdb_file, schema)
-
-
-# Define the SQL file containing the table creation script
-sql_file = "play_by_play_setup.sql"
